1bdbb64ead12e840571d4f007002bb619e658cf5
chris <chris314159@gmail.com>
2014-12-23 20:36:35 -0600

    solution for 1

--- prob1.py
+++ prob1.py
@@ -3,4 +3,9 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-print 3+4
+result = sum([x for x in range(1000) if (x%3 == 0 or x%5 == 0)])
+print("result is %i" %result)
+
+# You are the 419302nd person to have solved this problem.
+
+
