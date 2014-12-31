ef113bd835efbc3561bfea1e34362c1c8a4369e2
chris <chris314159@gmail.com>
2014-12-31 17:59:12 -0600

    checks all digits

--- prob4.py
+++ prob4.py
@@ -3,15 +3,43 @@
 
 # need to test for a palindrome
 import math
+from itertools import repeat
+import sys
 
 def is_palindrone(num):
     snum = str(num)
     num_len = len(snum)
-    for index, c in enumerate(snum):
-        if not (c == snum[num_len - index - 1] or
+    for index, c in enumerate(snum, start=1):
+        if not (c == snum[num_len - index] or
                 index > math.ceil(float(num_len)/2.0)):
             return False
     return True
 
-test = 80121108
-print(is_palindrone(test))
+# largest number is 999*999
+start = 999
+found = False
+largest = 0
+
+for i in range(start, 0, -1):
+    for n in zip(repeat(i), range(start, 0, -1)):
+        print(n)
+        product = n[0] * n[1]
+        if is_palindrone(product):
+            print("FOUND one... %i" %product)
+            if product > largest:
+                largest = product
+
+print("largest = %i" %largest)
+
+# for products in map(lambda x:(x, x*x, x*(x-1)), range(mult1, 0, -1)):
+#     n = products[0]
+#     print(n)
+#     print(n-1)
+#     print(n*n)
+#     print(n*(n-1))
+#     if is_palindrone(products[1]):
+#         print("FOUND!!!! %i" %products[1])
+#         break
+#     elif is_palindrone(products[2]):
+#         print("FOUND!!!! %i" %products[2])
+#         break
