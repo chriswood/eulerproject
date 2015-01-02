16801a17797f90361b369b35c876a7f6aa68891a
chris <chris314159@gmail.com>
2015-01-01 21:39:33 -0600

    Reads a large number in and finds largest product from various slices.

--- prob8.py
+++ prob8.py
@@ -6,6 +6,20 @@ str_num = ''
 with open('data8.txt', 'r') as f:
     for line in f:
         str_num += line
-	str_num = str_num.strip("\n")
-    print(str_num)
+    str_num = "".join(str_num.split('\n'))
 f.closed
+
+lproduct = 0
+num_len = 1000
+test_len = 13
+for n in range(num_len + 1 - test_len):
+    test = str_num[n:n+test_len]
+
+    #no need to test if zeroes
+    if test.find('0') < 0:
+        product = 1
+        for i in test:
+            product = product * int(i)
+        lproduct = product if (product > lproduct) else lproduct
+
+print("largest product is ", lproduct)
