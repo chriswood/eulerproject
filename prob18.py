e899b38ae373c22c8ec3cba22ec42ce14a8035c9
chris <chris314159@gmail.com>
2015-01-09 17:33:19 -0600

    Implemented a breadth first search and a queue and tree class to boot.

--- prob18.py
+++ prob18.py
@@ -1,71 +1,62 @@
+#python3.4
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def show(self):
+        print([x.value for x in self.items])
+
+    def clear(self):
+        self.items.clear()
+
+    def is_empty(self):
+        return(len(self.items) == 0)
 
 class Tree:
     def __init__(self, data):
         self.tree = data
-        self.height = len(data)
-        self.nodelist = []
 
-    def build_from_file(filename):
-        l = []
-        with open(filename, 'r') as f:
-            for line in f.readlines():
-                line = line.strip("\n")
-                l.append(map(int, line.split(" ")))
-        return l
+    def visit(self, node):
+        print("noode", node.value, sep=' ')
 
-#    def node(self, level, position):
-#        node = Node(level, position)
-
-    def add_node(self, level, pos):
-        self.nodelist.append((level, pos))
-
-    def display(self):
-        for n in self.nodelist:
-            print(n)
 
 class Node:
-    tree = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]
-    def __init__(self, level, position):
-        self.level = level
-        self.pos = position
-        self.value = self.tree[level][position]
-        self.l_value = self.tree[level + 1][position]
-        self.r_value = self.tree[level + 1][position + 1]
-        self.l_cost = self.value + self.l_value
-        self.r_cost = self.value + self.r_value
-        self.l = (self.level + 1, self.pos, self.l_cost)
-        self.r = (self.level + 1, self.pos + 1, self.r_cost)
-
-
-
-
-# nodes named nodelevel-pos
+    def __init__(self, nodedata): #(level, pos)
+        self.level = nodedata[0]
+        self.pos = nodedata[1]
+        self.value = tree.tree[self.level][self.pos]
+        try:
+            self.l_value = tree.tree[self.level + 1][self.pos]
+            self.r_value = tree.tree[self.level + 1][self.pos + 1]
+            self.l = Node((self.level + 1, self.pos))
+            self.r = Node((self.level + 1, self.pos + 1))
+        except(IndexError):
+            self.l = None
+            self.r = None
 #   3
 #  7 4
 # 2 4 6
 #8 5 9 3
-#find best of the bottom two rows.
-node = Node(2,0)
-path = []
-path.append((node.level, node.pos, node.value))
-if node.l_cost > node.r_cost:
-    path.append(node.l)
-else:
-    path.append(node.r)
-print(path)
+
+data = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]
+tree = Tree(data)
+root = Node((0, 0))
+q = Queue()
+q.enqueue(root)
+while not q.is_empty():
+    q.show()
+    node = q.dequeue()
+    if node.l:
+        q.enqueue(node.l)
+    if node.r:
+        q.enqueue(node.r)
 
 
 
-
-# breadth first traversal
-#levelorder(root)
-#  q = empty queue
-#  q.enqueue(root)
-#  while not q.empty do
-#    node := q.dequeue()
-#    visit(node)
-#    if node.left ≠ null then
-#      q.enqueue(node.left)
-#    if node.right ≠ null then
-#      q.enqueue(node.right)
