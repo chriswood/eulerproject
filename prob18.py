a42f6f9a8cfd8e2af8c2c69246f649f3a6712256
chris <chris314159@gmail.com>
2015-01-09 11:40:48 -0600

    I'm floundering on this problem but I think this is the right algorith to use, the breadth first traversal and I will add A* heuristic (sp?) function after this works.

--- prob18.py
+++ prob18.py
@@ -1,35 +1,60 @@
 
 
-# file handling
-#def read_file_to_list(filename):
-#    l = []
-#    with open(filename, 'r') as f:
-#        for line in f.readlines():
-#            line = line.strip("\n")
-#            l.append(map(int, line.split(" ")))
-#    return l
-
-#pyr = read_file_to_list('data18.txt')
-
-
 class Tree:
     def __init__(self, data):
         self.tree = data
         self.height = len(data)
+        self.nodelist = []
+
+    def build_from_file(filename):
+        l = []
+        with open(filename, 'r') as f:
+            for line in f.readlines():
+                line = line.strip("\n")
+                l.append(map(int, line.split(" ")))
+        return l
+
+#    def node(self, level, position):
+#        node = Node(level, position)
+
+    def add_node(self, level, pos):
+        self.nodelist.append((level, pos))
+
+    def display(self):
+        for n in self.nodelist:
+            print(n)
 
 class Node:
+    tree = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]
     def __init__(self, level, position):
         self.level = level
         self.pos = position
+        self.value = self.tree[level][position]
+        self.l_value = self.tree[level + 1][position]
+        self.r_value = self.tree[level + 1][position + 1]
+        self.l_cost = self.value + self.l_value
+        self.r_cost = self.value + self.r_value
+        self.l = (self.level + 1, self.pos, self.l_cost)
+        self.r = (self.level + 1, self.pos + 1, self.r_cost)
 
-test = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]
-tree = Tree(test)
-print(tree.tree)
 
+
+
+# nodes named nodelevel-pos
 #   3
 #  7 4
 # 2 4 6
 #8 5 9 3
+#find best of the bottom two rows.
+node = Node(2,0)
+path = []
+path.append((node.level, node.pos, node.value))
+if node.l_cost > node.r_cost:
+    path.append(node.l)
+else:
+    path.append(node.r)
+print(path)
+
 
 
 
