3947a35ff6cd359aa8ea0e732277bbeae6feb3f7
chris <chris314159@gmail.com>
2015-01-11 13:41:24 -0600

    This was by far the easier solution to this problem. Paths are represented as binary numbers, which python handles nicely.

--- prob18.py
+++ prob18.py
@@ -1,27 +1,20 @@
 #python3.4
 
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def spop(self):
-        return self.items.pop()
-
-    def show(self):
-        print([x.value for x in self.items])
-
-    def clear(self):
-        self.items.clear()
-
-    def is_empty(self):
-        return(len(self.items) == 0)
+# file handling
+def read_file_to_list(filename):
+    l = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            line = line.strip("\n")
+            l.append([int(x) for x in line.split(" ")])
+        return l
 
 class Tree:
     def __init__(self, data):
         self.tree = data
+        self.length = len(data)
+        self.high_cost = 0
+        self.high_path = ''
 
     def visit(self, node):
         node.visit()
@@ -29,59 +22,36 @@ class Tree:
     def bottom(self, level):
         return(level == (len(data) - 1))
 
-class Node:
-    def __init__(self, nodedata): #(level, pos)
-        self.level = nodedata[0]
-        self.pos = nodedata[1]
-        self.value = tree.tree[self.level][self.pos]
-        self.visited = False
-        if not tree.bottom(self.level):
-            self.l = (self.level + 1, self.pos)
-            self.r = (self.level + 1, self.pos + 1)
-        else:
-            self.l = None
-            self.r = None
-
-    def visit(self):
-        self.visited = True
-
-#   3
-#  7 4
-# 2 4 6
-#8 5 9 3
-
-data = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]
+    def traverse(self, path):
+        print("Path: ", path)
+        clevel = 0
+        cpos = 0
+        cost = self.tree[clevel][cpos]
+        for c in path:
+            new_level = clevel + 1
+            if c == '0':
+                cost += self.tree[new_level][cpos]
+            elif c == '1':
+                new_pos = cpos + 1
+                cost += self.tree[new_level][new_pos]
+                cpos = new_pos
+            clevel = new_level
+        if cost > self.high_cost:
+            self.high_cost = cost
+            self.high_path = path
+            
+data = read_file_to_list('data18.txt')
+print(data)
 tree = Tree(data)
-root = Node((0, 0))
-tree.visit(root)
-s = Stack()
-s.push(Node(root.l))
-s.push(Node(root.r))
-while not s.is_empty():
-    node = s.spop()
-    print(node.value)
-    if node.l:
-        lnode = Node(node.l)
-        if not lnode.visited:
-            tree.visit(lnode)
-            s.push(lnode)
-    if node.r:
-        rnode = Node(node.r)
-        if not rnode.visited:
-            tree.visit(rnode)
-            s.push(rnode)
-
-
-#iterative DFS(Vertex v)
-#    mark v visited
-#    make an empty Stack S
-#    push all vertices adjacent to v onto S
-#    while S is not empty do
-#        Vertex w is pop off S
-#        for all Vertex u adjacent to w do
-#            if u is not visited then
-#                mark u visited
-#                push u onto S
-
+pathcount = pow(2, tree.length - 1)
+for n in range(pathcount):
+    path = bin(n) #binary string
+    path = path[2:] #remove 'b' notation
+    path = path.zfill(tree.length - 1)
+    tree.traverse(path)
+    if n >= pathcount:
+        break
+print("Cost: ", tree.high_cost)
+print("Path: ", tree.high_path)
 
 
