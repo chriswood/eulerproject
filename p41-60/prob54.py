a3b36f4ee3efb18e81b5f5604e945ecfbcc6e323
chriswood <chris314159@gmail.com>
2015-05-16 00:05:36 -0500

    some cool poker game handling

--- p41-60/prob54.py
+++ p41-60/prob54.py
@@ -5,4 +5,55 @@ def read_hands(filename):
         for hand in f:
             print(hand)
 
-read_hands('../data/p054_poker.txt')
+# read_hands('../data/p054_poker.txt')
+test = 'AS TS QS JS KS 7C 8C 5C QD 6C'
+
+class Hand:
+    def __init__(self, cards):
+        self.p1 = cards.split()[:5]
+        self.p2 = cards.split()[5:]
+        self.classes = ['rf', 'sf', 'fk']
+        self.order = '23456789TJQKA'
+
+    def display(self):
+        print(self.p1)
+
+    def sort_hand(self):
+        pass
+
+    def classify(self):
+        for fname in self.classes:
+            f = getattr(self, fname)
+            if f():
+                return f.__name__
+
+    def rf(self):
+        match = sorted(set(self.order[-5:]))
+        return (self.is_flush(self.p1) and
+            sorted(set(''.join([x[0] for x in self.p1]))) == match)
+
+    def sf(self):
+        return False
+
+    def fk(self):
+        return True
+
+    def is_flush(self, suits):
+        # takes string of suits
+        return True
+
+hand = Hand(test)
+hand.display()
+hand_type = hand.classify()
+print(hand_type)
+
+    # High Card: Highest value card.
+    # One Pair: Two cards of the same value.
+    # Two Pairs: Two different pairs.
+    # Three of a Kind: Three cards of the same value.
+    # Straight: All cards are consecutive values.
+    # Flush: All cards of the same suit.
+    # Full House: Three of a kind and a pair.
+    # Four of a Kind: Four cards of the same value.
+    # Straight Flush: All cards are consecutive values of same suit.
+    # Royal Flush: Ten, Jack, Queen, King, Ace, in same suit.
