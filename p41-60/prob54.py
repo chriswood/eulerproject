222a6cdf8cbb46fb809b679fe3e19800b4bd5916
chriswood <chris314159@gmail.com>
2015-05-18 13:00:37 -0500

    Adding smarter straight and flush logic to handle ties.

--- p41-60/prob54.py
+++ p41-60/prob54.py
@@ -1,4 +1,4 @@
-from itertools import dropwhile
+from collections import Counter
 
 def read_hands(filename):
     with open(filename, 'r') as f:
@@ -6,14 +6,15 @@ def read_hands(filename):
             print(hand)
 
 # read_hands('../data/p054_poker.txt')
-test = '8S TS QS 9S JS 7C 8C 5C QD 6C'
+test = '8S JC 7S 9H TD 7C 8C 5C QD 6C'
 
 class Hand:
     def __init__(self, cards):
         self.p1 = cards.split()[:5]
         self.p2 = cards.split()[5:]
-        self.classes = ['rf', 'sf', 'fk']
+        self.classes = ['rf', 'sf', 'fk', 'fh', 'fl', 'st']
         self.order = '23456789TJQKA'
+        self.key1_card = None
 
     def display(self):
         print(self.p1)
@@ -36,7 +37,39 @@ class Hand:
         return self.is_straight(self.p1) and self.is_flush(self.p1)
 
     def fk(self):
-        return True
+        return self.is_pair(self.p1, 4)
+
+    def fh(self):
+        return self.is_pair(self.p1, 0, fh_check=True)
+
+    def fl(self):
+        if self.is_flush(self.p1):
+            self.key1_card = self.high_card(self.p1)
+            return True
+        return False
+
+    def st(self):
+        if self.is_straight(self.p1):
+            self.key1_card = self.high_card(self.p1)
+            print(self.key1_card)
+            return True
+        return False
+
+    def high_card(self, cards):
+        return max(cards, key=lambda x: self.order.index(x[0]))
+
+    def is_pair(self, cards, pair_count, fh_check=False):
+        # pair_count = 4 for 4 of a kind, etc...
+        groups = Counter(x[0] for x in cards)
+        res = groups.most_common()
+        if fh_check:
+            if len(res) == 2 and res[0][1] == 3:
+                self.key1_card = (res[0][0], res[1][0])
+                return True
+        elif res[0][1] == pair_count:
+            self.key1_card = res[0][0]
+            return True
+        return False
 
     def is_flush(self, cards):
         return len([x for x in cards if x[1] == cards[0][1]]) == len(cards)
@@ -44,7 +77,6 @@ class Hand:
     def is_straight(self, cards):
         start = self.lowest_card(cards)
         match = sorted(set(self.order[start:start + 5]))
-        print(sorted(set([x[0] for x in cards])))
         return match == sorted(set([x[0] for x in cards]))
 
     def lowest_card(self, cards):
@@ -54,6 +86,8 @@ class Hand:
             if pos < lowest:
                 lowest = pos
         return lowest
+
+
 
 hand = Hand(test)
 hand.display()
