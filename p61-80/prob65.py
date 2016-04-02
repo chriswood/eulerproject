bbe6a57dd336d795a2d9a13251745249f9a8e2dd
chriswood <chris314159@gmail.com>
2016-04-02 10:41:39 -0500

    Floating point representation of convergence to e via continued fractions

---
+++ p61-80/prob65.py
@@ -0,0 +1,31 @@
+from itertools import cycle
+
+def k_sieve():
+    yield 2
+    yield 1
+    k = 1
+    multipliers = cycle([2,1,1])
+    while 1:
+        n = next(multipliers)
+        if n == 2:
+            n = k * n
+            k += 1
+        yield n
+
+#e = [2; 1,2,1, 1,4,1, 1,6,1 , ... , 1,2k,1, ...].
+
+def cf(k, terms):
+    '''if we are n(terms) levels deep, then return 1/k(n+1) + k(n):
+       else return k(n) + 1/cf(k(n+1)'''
+    print("called with k,terms=", k,terms)
+    if terms == 1:
+        return k
+    elif terms == 2:
+        return k + (1/next(k_terms))
+    else:
+        return k + 1/(cf(next(k_terms), terms - 1))
+
+term_num = 5
+k_terms = k_sieve()
+e = cf(next(k_terms), term_num)
+print("For", term_num, "terms, e is approximately: ", e)
