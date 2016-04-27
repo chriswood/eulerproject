4c474cfc8485690bc50e03802be26dd4a3c69e78
chriswood <chris314159@gmail.com>
2016-04-27 16:46:55 -0500

    MUCH  quicker solution for 6 digit magic ring problem

--- p61-80/prob68.py
+++ p61-80/prob68.py
@@ -16,8 +16,9 @@
 # each group of n digits should add up to the same number
 # zeroes are always part of a ten
 #first number of first group should be lower than first number of other groups
+import sys
 from string import digits
-from itertools import zip_longest
+from itertools import zip_longest, permutations
 from collections import Counter
 import pprint
 
@@ -33,7 +34,7 @@ def grouper(iterable, n):
     print(list(args[0]))
     return zip(*args)
 
-def test(n):
+def test(s):
     '''n = size of polygon
        -16 digits,  10,9,8,7,6,5,4,3,2,1
        -each group of 3 must add up to the same thing
@@ -41,39 +42,52 @@ def test(n):
             no more than twice in the number, and only twice in certain spots
        -the first digit must be the lowest of any first digit of the sets of three
        abc,dce,feg,hgi,jib'''
-    print("testing {0} with digits {1}".format(n, d))
-    s = str(n)
+    #print("testing {0} with digits {1}".format(n, d))
+    #s = str(n)
     #ns = grouper(s, line_size)
-
+    #for 16 digits there must be one 10, and it can't be in position  of repeat
     #test 1 - sums
-    for i in range(0, ans_size, line_size):
+    for i in range(0, ans_size + 1, line_size):
         a = sum(map(int, s[i:i + line_size]))
         if i == 0:
             first = a    
         if a is not first:
-            print("sums don't check out")
+            #print("sums don't check out")
             return False
 
     #test 2 - right counts of digits
     tcount = Counter(s)
     if len(tcount) is not ans_size:
-        print("wrong number of digits")
+        #print("wrong number of digits")
         return False
     #test 3 - each number used only twice at the most
     if tcount.most_common(1)[0][1] > 2:
-        print("too many {0}'s".format(tcount.most_common(1)[0][0]))
+        #print("too many {0}'s".format(tcount.most_common(1)[0][0]))
         return False
-    # for 6 digits
-    #abcdcefeb
-    print("here",s)
+    # for 6 digits, abcdcefeb
+    # for 16,  abc,dce,feg,hgi,jib 
     position_tests = [s[4] == s[2], s[7] == s[5], s[8] == s[1]]
     if not all(position_tests):
-        print("positions wrong, JACKASS")
+        #print("positions wrong, JACKASS")
         return False
 
     return True
-#r = digits
-#test(432621513)
-print(test(423513612))
 
-# First we need a test to see if a solution is valid
+def arrange(s):
+    #123456
+    return s[:4] + s[2] + s[4] + s[5] + s[4] + s[1]
+
+
+perms = permutations('123456')
+for p in perms:
+    s = ''.join(p)
+    ring_f = arrange(s)
+    if test(ring_f):
+        print("found one!!!", ring_f)
+
+# for each permutation of 123456,
+# check with the specific order
+# for 123456
+# 123435652
+
+# we could list all initial sets of 3 that add up to a certain sum, then check those
